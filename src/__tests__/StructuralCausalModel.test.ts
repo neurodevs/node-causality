d2ffa829b85b6945deacff43911b6ceef1540079
ericthecurious <hello@ericthecurious.com>
2025-07-13 23:38:41 -0700

    minor: integrate causal model triple U, V, F (still yolo)

--- src/__tests__/StructuralCausalModel.test.ts
+++ src/__tests__/StructuralCausalModel.test.ts
@@ -1,6 +1,7 @@
 import AbstractSpruceTest, { test, assert } from '@sprucelabs/test-utils'
 import StructuralCausalModel, {
     CausalModel,
+    CausalModelOptions,
 } from '../modules/StructuralCausalModel'
 
 export default class StructuralCausalModelTest extends AbstractSpruceTest {
@@ -17,7 +18,24 @@ export default class StructuralCausalModelTest extends AbstractSpruceTest {
         assert.isTruthy(this.instance, 'Could not create instance!')
     }
 
+    @test()
+    protected static async toTripleMethodReturnsCorrectTriple() {
+        const triple = this.instance.toTriple()
+
+        assert.isEqualDeep(
+            triple,
+            this.expectedTriple,
+            'Triple does not equal expected!'
+        )
+    }
+
+    private static expectedTriple: CausalModelOptions = {
+        U: new Set(),
+        V: new Set(),
+        F: new Set(),
+    }
+
     private static StructuralCausalModel() {
-        return StructuralCausalModel.Create()
+        return StructuralCausalModel.Create(this.expectedTriple)
     }
 }
--- src/modules/StructuralCausalModel.ts
+++ src/modules/StructuralCausalModel.ts
@@ -1,13 +1,45 @@
 export default class StructuralCausalModel implements CausalModel {
     public static Class?: CausalModelConstructor
 
-    protected constructor() {}
+    private U: Set<ExternalVariable>
+    private V: Set<InternalVariable>
+    private F: Set<CausalMechanism>
 
-    public static Create() {
-        return new (this.Class ?? this)()
+    protected constructor(options: CausalModelOptions) {
+        const { U, V, F } = options
+
+        this.U = U
+        this.V = V
+        this.F = F
+    }
+
+    public static Create(options: CausalModelOptions) {
+        return new (this.Class ?? this)(options)
+    }
+
+    public toTriple() {
+        return {
+            U: this.U,
+            V: this.V,
+            F: this.F,
+        }
     }
 }
 
-export interface CausalModel {}
+export interface CausalModel {
+    toTriple(): CausalModelOptions
+}
+
+export interface CausalModelOptions {
+    U: Set<ExternalVariable>
+    V: Set<InternalVariable>
+    F: Set<CausalMechanism>
+}
+
+export interface ExternalVariable {}
+
+export interface InternalVariable {}
+
+export interface CausalMechanism {}
 
 export type CausalModelConstructor = new () => CausalModel
